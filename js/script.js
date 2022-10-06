d66c5efc31e832cde29497628aab7c8250ffebbd
andreyko21 <112313332+andreyko21@users.noreply.github.com>
2022-10-06 15:14:04 +0300

    v4

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
 var swiper1 = new Swiper(".swiper", {
-    effect: "cards",
-    grabCursor: true,
-    navigation: {
-        nextEl: ".swiper-next",
-        prevEl: ".swiper-prev",
-      },
-  });
+  effect: "cards",
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
 
-  var swiper2 = new Swiper(".swiper-2", {
-    watchSlidesProgress: true,
-    watchOverflow: true,
-    breakpoints: {
-      1250: {
-        slidesPerView: 4,
+var swiper2 = new Swiper(".swiper-2", {
+  watchSlidesProgress: true,
+  watchOverflow: true,
+  breakpoints: {
+    1250: {
+      slidesPerView: 4,
     },
-      768: {
-          slidesPerView: 1.5,
-      },
-      576: {
-        slidesPerView: 1.1,
-      }
+    768: {
+      slidesPerView: 1.5,
     },
-  });
- 
+    576: {
+      slidesPerView: 1.1,
+    }
+  },
+});
 
-  var swiper3 = new Swiper(".swiper-3", {
+
+var swiper3 = new Swiper(".swiper-3", {
   autoHeight: false,
   slidesPerColumn: 2,
   spaceBetween: 30,
   breakpoints: {
-    1250:{
-    slidesPerView: 4,
-    grid: {
-      rows: 2,
+    1250: {
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+      },
     },
-  },
     768: {
-        slidesPerView: 2.5,
-        grid: {
-          rows: 2,
-        },
+      slidesPerView: 2.5,
+      grid: {
+        rows: 2,
+      },
     },
     576: {
       slidesPerView: 2,
@@ -48,40 +48,40 @@ var swiper1 = new Swiper(".swiper", {
       },
     }
   },
-  });
-  const iconMenu2 = document.querySelector('.nav__menu');
-  const iconMenu = document.querySelector('.burger-menu');
-if(iconMenu){
-    const menuHeader = document.querySelector('.header');
-    const menuBlocks = document.querySelector('.menu-blocks');
-    iconMenu.addEventListener("click", function(e){
-        document.body.classList.toggle('lock');
-        iconMenu.classList.toggle('active');
-        menuBlocks.classList.toggle('active');
-        menuHeader.classList.toggle('active');
-    })   
-}
-if(iconMenu2){
+});
+const iconMenu2 = document.querySelector('.nav__menu');
+const iconMenu = document.querySelector('.burger-menu');
+if (iconMenu) {
   const menuHeader = document.querySelector('.header');
-    const menuBlocks = document.querySelector('.menu-blocks');
-    iconMenu2.addEventListener("click", function(e){
-        document.body.classList.remove('lock');
-        iconMenu2.classList.remove('active');
-        menuBlocks.classList.remove('active');
-        menuHeader.classList.remove('active');
-    })  
+  const menuBlocks = document.querySelector('.menu-blocks');
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuBlocks.classList.toggle('active');
+    menuHeader.classList.toggle('active');
+  })
+}
+if (iconMenu2) {
+  const menuHeader = document.querySelector('.header');
+  const menuBlocks = document.querySelector('.menu-blocks');
+  iconMenu2.addEventListener("click", function (e) {
+    document.body.classList.remove('lock');
+    iconMenu2.classList.remove('active');
+    menuBlocks.classList.remove('active');
+    menuHeader.classList.remove('active');
+  })
 }
 
 var header = $('.header'),
-		scrollPrev = 0;
+  scrollPrev = 0;
 
-$(window).scroll(function() {
-	var scrolled = $(window).scrollTop();
- 
-	if ( scrolled > 100 && scrolled > scrollPrev ) {
-		header.addClass('out');
-	} else {
-		header.removeClass('out');
-	}
-	scrollPrev = scrolled;
+$(window).scroll(function () {
+  var scrolled = $(window).scrollTop();
+
+  if (scrolled > 100 && scrolled > scrollPrev) {
+    header.addClass('out');
+  } else {
+    header.removeClass('out');
+  }
+  scrollPrev = scrolled;
 });
